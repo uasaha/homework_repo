5c04f379bdeea4665c0c5d5a6244d3f855529f5b
운석 <istana000@gmail.com>
2022-10-07 16:34:38 +0900

    multi package 변경, main함수에 연산 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
 
 public class Main {
     public static void main(String[] args){
-
+        addNumber add = new addNumber();
+        Multi multi = new Multi();
         final int a = 10;
         final int b = 2;
 
-        System.out.println("");
+        System.out.println(add.add(a, b));
+        System.out.println(multi.Multi(a, b));
+
+
     }
 }
--- src/main/java/Multi.java
+++ src/main/java/Multi.java
@@ -1,5 +1,3 @@
-package main.java;
-
 class Multi{
     public int Multi(int num1, int num2){
         return num1 * num2;
