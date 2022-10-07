0722801e17bf8dbdc07913a1eaff08467e41dff2
운석 <istana000@gmail.com>
2022-10-07 16:42:01 +0900

    모든 클래스 합쳐서 Main 마무리

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,16 @@ public class Main {
     public static void main(String[] args){
         addNumber add = new addNumber();
         Multi multi = new Multi();
+        divNumber div = new divNumber();
+        SubNumber sub = new SubNumber();
+
         final int a = 10;
         final int b = 2;
 
         System.out.println(add.add(a, b));
         System.out.println(multi.Multi(a, b));
-
+        System.out.println(div.divNum(a, b));
+        System.out.println(sub.subNumber(a, b));
 
     }
 }
